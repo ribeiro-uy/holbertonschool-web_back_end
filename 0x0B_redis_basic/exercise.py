b3b845f7f6f9c0c987ac1505fef9276ab2ee90d0
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2022-03-15 20:15:35 -0300

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,8 @@
 """exercise"""
 import redis
 from uuid import uuid4
-from typing import Union
-
-Types = Union[str, bytes, int, float]
+from typing import Union, Optional, Callable
+TYPES = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -13,8 +12,22 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Types) -> str:
+    def store(self, data: TYPES) -> str:
         """Store the input data in Redis"""
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> TYPES:
+        """Get key from redis"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: bytes) -> str:
+        """Parametrize Cache.get as string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Parametrize Cache.get as int"""
+        return self.get(key, int)
